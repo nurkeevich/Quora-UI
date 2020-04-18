dac8cf7b9fa1bde2e256f55b7ac89185ecd337a9
Tilekbek Kadyrov <tilekbekkadyrov@gmail.com>
2020-04-18 00:45:22 -0500

    now we can run react app with parcel, redux ts

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const ROOT = document.getElementById("root");
+ReactDOM.render(<App />, ROOT);
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppRoutes = () => {
+    return (
+        <div>
+            <h1>App Routes</h1>
+        </div>
+    );
+};
+
+export default AppRoutes;
